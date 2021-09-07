f6b2edc414f3e4211335359aca0e4b2315503857
Joe Lanzi <joelanzi@ymail.com>
2021-09-07 13:19:56 -0500

    Update spell.py

--- version_0_1/spell.py
+++ version_0_1/spell.py
@@ -7,13 +7,14 @@
 
 # sequence matcher ??? diflib 
 # tokenization ???
+
 #%% Using Language detection
 from langdetect import detect
 
 print(detect("War doesn't show who's right, just who's left."))
 print(detect("Ein, zwei, drei, vier"))
 
-# %% Using pyenchange as a spellchecking library
+# %% Using pyenchange as a spellchecking library and suggestion function
 import enchant
 #help(enchant)
 
