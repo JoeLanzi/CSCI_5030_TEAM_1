73773d03cd042c3be3b2340c2c915a673f70c4cf
Joe Lanzi <joelanzi@ymail.com>
2021-09-15 17:38:20 -0500

    added lang detect + enchant spell check

--- spell.py
+++ spell.py
@@ -8,17 +8,49 @@
 # sequence matcher ??? diflib 
 # tokenization ???
 
+# check language -> load dict -> check spelling -> tokenization
 #%% Using Language detection
+from json import detect_encoding
+from random import sample
 from langdetect import detect
+from langdetect.detector_factory import detect_langs
 
 print(detect("War doesn't show who's right, just who's left."))
 print(detect("Ein, zwei, drei, vier"))
 
-# %% Using pyenchange as a spellchecking library and suggestion function
+# %% Using pyenchant as a spellchecking library
 import enchant
 #help(enchant)
 
 checker = enchant.Dict("en_US")
 print(checker.check("test"))
 print(checker.check("onomonipia"))
+# %%
+from langdetect import detect
+import enchant
+
+def spell_sheck(input):
+    if detect(input) == 'en':
+        test = input.split()
+        count = 0
+        errors = []
+        checker = enchant.Dict("en_US")
+        for item in test:
+            if checker.check(item) == False:
+                errors.append(item)
+                test[count] = checker.suggest(item)[0]
+            count+=1
+        print(" ".join(test))
+        return errors 
+
+sample_str = "Hello my namr justlyt is Joe and I live near Chesterfield MO"
+spell_sheck(sample_str)
+
+# %%
+str = "Hello world"
+str.split()
+# %%
+str = "hellow"
+checker = enchant.Dict("en_US")
+checker.suggest(str)[0]
 # %%
